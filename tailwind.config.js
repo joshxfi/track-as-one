1e2eea5718c8e8136db6841552f0717f318a8eb6
Josh Daniel <joshpersonal8@gmail.com>
2021-09-14 11:52:07 +0800

    Enable `jit` mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 module.exports = {
-  purge: [],
+  mode: 'jit',
+  purge: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bgwhite': '#F9F9F9',
+        'primary': '#2B2B2B',
+        'secondary': '#E3B04B' 
+      }
+    },
   },
   variants: {
     extend: {},
