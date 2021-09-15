fbbfef97fa23ff43a10b54d21fb72480ead6a115
Josh Daniel <joshpersonal8@gmail.com>
2021-09-15 12:22:14 +0800

    Add join room route

---
+++ pages/join.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Header } from '../components/Header';
+import { VscSignIn } from 'react-icons/vsc';
+
+const Join = () => {
+  return (
+    <section className='wrap'>
+      <Header title='Join a Room' />
+      <form className='w-full flex justify-center flex-col'>
+        <input
+          className='h-[36px] bg-inputbg rounded-[36px] px-[20px] text-sm outline-none border-2 focus:border-primary'
+          type='text'
+          placeholder='enter room id'
+        />
+        <div className='inline-block mx-auto mt-6'>
+          <button className='btn'>
+            <p className='mr-2'>join room</p>
+            <VscSignIn className='icon' />
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default Join;
