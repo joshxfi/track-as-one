97a4ff780b4a8cf136b4e37e2e6b52f237fd0481
Josh Daniel <joshpersonal8@gmail.com>
2021-09-15 12:22:39 +0800

    Convert header to a component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
-export const Header = () => {
+interface HeaderProps {
+  title?: string;
+  desc?: string;
+}
+
+export const Header: React.FC<HeaderProps> = ({ title, desc }) => {
   return (
     <header className='mt-12 text-center mb-4'>
-      <h1 className='text-5xl font-bold'>trackAsOne</h1>
-      <i className='text-base'>organize your tasks as one</i>
+      <h1 className='text-5xl font-bold'>{title}</h1>
+      <i className='text-base'>{desc}</i>
     </header>
   );
+};
+
+Header.defaultProps = {
+  title: 'trackAsOne',
+  desc: 'organize your tasks as one',
 };
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import Link from 'next/link';
 import { FaSignInAlt } from 'react-icons/fa';
 
 export const Navbar: React.FC = () => {
   return (
     <nav className='w-full bg-primary py-3 text-f9'>
       <div className='flex justify-between items-center w-[85%] mx-auto'>
-        <h1 className='text-secondary font-bold text-3xl'>
-          tAO<span className='text-f9'>.</span>
-        </h1>
+        <Link href='/homepage'>
+          <h1 className='text-secondary font-bold text-3xl'>
+            tAO<span className='text-f9'>.</span>
+          </h1>
+        </Link>
         <div className='flex items-center text-sm'>
           <p className='text-sm mr-2'>sign in</p>
           <FaSignInAlt className='text-secondary' />
