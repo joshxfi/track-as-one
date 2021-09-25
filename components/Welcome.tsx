75d5dd186f0bf9827f1c58c04a5bdf0e79a4aa8c
Josh Daniel <joshpersonal8@gmail.com>
2021-09-25 15:06:39 +0800

    Feat: add new section at welcome page

--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import Image from 'next/image'
 import avatar from '../public/assets/avatar.svg'
-import { FaSignInAlt } from 'react-icons/fa'
+import avatar2 from '../public/assets/avatar2.svg'
+import { FaSignInAlt, FaHandHoldingHeart, FaPhone } from 'react-icons/fa'
 import { useAuth } from '../context/AuthContext'
 
 export const Welcome: React.FC = () => {
@@ -9,8 +10,8 @@ export const Welcome: React.FC = () => {
 
   return (
     <>
-      <section className='wrap h-screen lg:container lg:items-start lg:pt-16 lg:flex-row lg:justify-between'>
-        <div className='mt-8 lg:flex lg:justify-between lg:flex-col lg:items-start text-center'>
+      <section className='wrap h-screen lg:container lg:items-start lg:pt-28 lg:flex-row lg:justify-between'>
+        <div className='pt-8 lg:flex lg:justify-between lg:flex-col lg:items-start text-center'>
           <h1 className='text-5xl font-bold md:text-7xl xl:text-8xl lg:mb-2'>
             trackAs
             <span className='gradient-text'>One</span>
@@ -49,6 +50,32 @@ export const Welcome: React.FC = () => {
 
         <div className='w-[400px] hidden lg:block pt-12'>
           <Image src={avatar} objectFit='contain' alt='trackAsOne avatar' />
+        </div>
+      </section>
+
+      <section className='lg:flex lg:justify-between lg:h-[80vh]'>
+        <div className='text-center lg:text-left lg:pl-2'>
+          <h1 className='text-2xl font-semibold lg:font-medium lg:text-4xl'>
+            Built by a student for students.
+          </h1>
+          <p className='mt-2 text-lg lg:w-[490px] xl:w-[600px] xl:text-xl'>
+            trackAsOne is a collaborative cross-platform app that enables
+            students to track homework, activities, and more together with their
+            friends or classmates.
+          </p>
+          <div className='flex flex-col lg:flex-row items-center'>
+            <button className='lg:mr-5 rounded-[36px] bg-gradient-to-tr from-secondary to-[#FFDC54] p-2 w-[230px] px-[50px] font-semibold mt-4 btnEffect flex-between'>
+              <p className='mr-4'>contribute</p>{' '}
+              <FaHandHoldingHeart className='text-lg' />
+            </button>
+            <button className='rounded-[36px] bg-gradient-to-tr from-secondary to-[#FFDC54] p-2 w-[230px] px-[50px] font-semibold mt-4 btnEffect flex-between'>
+              <p className='mr-4'>contact me</p> <FaPhone />
+            </button>
+          </div>
+        </div>
+
+        <div className='mt-8 w-[300px] lg:w-[400px] mx-auto lg:mx-0'>
+          <Image src={avatar2} objectFit='contain' alt='trackAsOne avatar' />
         </div>
       </section>
     </>
