e05e95bb0f000b79ea002053892a464774c487bd
Josh Daniel <joshpersonal8@gmail.com>
2021-09-26 15:15:46 +0800

    Fix: set input width to 95% on mobile view

--- components/Input.tsx
+++ components/Input.tsx
@@ -9,7 +9,7 @@ export const Input: React.FC<InputProps> = ({
     <input
       onChange={handleChange}
       value={value}
-      className='w-[450px] md:w-[550px] h-[36px] bg-inputbg rounded-[36px] px-[20px] text-sm outline-none border-2 focus:border-primary'
+      className='w-[95%] md:w-[550px] h-[36px] bg-inputbg rounded-[36px] px-[20px] text-sm outline-none border-2 focus:border-primary'
       type='text'
       placeholder={placeholder}
     />
