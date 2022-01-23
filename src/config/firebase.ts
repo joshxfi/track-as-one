8ab4d88414ce99475cf74dd124603152f874064f
Josh Daniel <joshpersonal8@gmail.com>
2022-01-23 22:07:06 +0800

    chore: initialize app on production

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,14 +2,18 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
-const firebaseApp = initializeApp({
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: 'trackas1.firebaseapp.com',
-  projectId: 'trackas1',
-  storageBucket: 'trackas1.appspot.com',
-  messagingSenderId: '672092906058',
-  appId: '1:672092906058:web:f3589aa30b780eb08a7cb3',
-});
+let firebaseApp: any;
+
+if (process.env.NEXT_PUBLIC_STATE === 'production') {
+  firebaseApp = initializeApp({
+    apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    authDomain: 'trackas1.firebaseapp.com',
+    projectId: 'trackas1',
+    storageBucket: 'trackas1.appspot.com',
+    messagingSenderId: '672092906058',
+    appId: '1:672092906058:web:f3589aa30b780eb08a7cb3',
+  });
+}
 
 const db = getFirestore(firebaseApp);
 const auth = getAuth(firebaseApp);
