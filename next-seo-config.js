065c803a666d527fbf688842a57380777a91074d
Josh Daniel <joshpersonal8@gmail.com>
2022-01-25 14:34:13 +0800

    feat: add default seo

---
+++ next-seo-config.js
@@ -0,0 +1,36 @@
+const description =
+  'A platform that enables students to track homework, activities, etc. together with their friends or classmates. With this platform, you can create rooms, invite your friends, and track tasks collaboratively!';
+
+export default {
+  title: 'trackAsOne',
+  description,
+  openGraph: {
+    type: 'website',
+    url: 'https://trackasone.me',
+    title: 'trackAsOne',
+    description,
+    images: [
+      {
+        url: 'https://firebasestorage.googleapis.com/v0/b/trackas1.appspot.com/o/seo%2Ftrack-as-one.png?alt=media&token=2e0f0e71-dde8-444d-b5d4-60626ea0a058',
+        width: 800,
+        height: 600,
+        alt: 'trackAsOne',
+        type: 'image/jpeg',
+      },
+      {
+        url: 'https://firebasestorage.googleapis.com/v0/b/trackas1.appspot.com/o/seo%2Ftrack-as-one.png?alt=media&token=2e0f0e71-dde8-444d-b5d4-60626ea0a058',
+        width: 900,
+        height: 800,
+        alt: 'trackAsOne Alt',
+        type: 'image/jpeg',
+      },
+      {
+        url: 'https://firebasestorage.googleapis.com/v0/b/trackas1.appspot.com/o/seo%2Ftrack-as-one.png?alt=media&token=2e0f0e71-dde8-444d-b5d4-60626ea0a058',
+      },
+    ],
+    site_name: 'trackAsOne',
+  },
+  twitter: {
+    cardType: 'summary_large_image',
+  },
+};
