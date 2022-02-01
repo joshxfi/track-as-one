5b12bcc480c988ff81093c82827cf3f9b57d9edf
Josh Daniel <joshpersonal8@gmail.com>
2022-02-01 10:09:54 +0800

    chore: add nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import '../styles/global.css';
 import '../styles/tailwind.css';
+import 'nprogress/nprogress.css';
+import Router from 'next/router';
+import NProgress from 'nprogress';
 import { DefaultSeo } from 'next-seo';
 import type { AppProps } from 'next/app';
 import { Toaster } from 'react-hot-toast';
@@ -8,6 +11,10 @@ import { AnimatePresence } from 'framer-motion';
 import { AuthProvider } from '@/context/AuthContext';
 
 import SEO from '../next-seo-config';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
