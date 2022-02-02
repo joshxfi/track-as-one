4edf6ae911c748c76ed69b0712359cb9f6736ea5
Josh Daniel <joshpersonal8@gmail.com>
2022-02-02 14:02:31 +0800

    style: update modal when deleting task

--- src/components/Room/Task.tsx
+++ src/components/Room/Task.tsx
@@ -1,28 +1,48 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useState } from 'react';
+
 import toast from 'react-hot-toast';
 import { FaLink } from 'react-icons/fa';
 import { IoCloseCircle } from 'react-icons/io5';
 import { BsFillCheckCircleFill } from 'react-icons/bs';
 
+import { Modal } from '@/components';
+import { db } from '@/config/firebase';
 import { Popup } from '@/components/Room';
 import { SideBtn } from '@/components/Button';
+import { useAuth } from '@/context/AuthContext';
+import { arrayUnion, deleteDoc, doc, updateDoc } from 'firebase/firestore';
 
 interface RoomTaskProps {
   task: ITask;
-  members: number;
-  isAdmin: boolean;
-  taskDone: (id: string) => void;
-  taskDel: (id: string) => void;
+  room: IRoom;
 }
 
-const RoomTask: React.FC<RoomTaskProps> = ({
-  task,
-  members,
-  isAdmin,
-  taskDone,
-  taskDel,
-}) => {
+const RoomTask: React.FC<RoomTaskProps> = ({ task, room }) => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const { data } = useAuth();
+
+  const isAdmin = room.creator === data.id;
+
+  const taskDone = async (id?: string) => {
+    setModalOpen(false);
+
+    const taskRef = doc(db, `rooms/${room?.id}/tasks/${id}`);
+    await updateDoc(taskRef, {
+      completedBy: arrayUnion(data.id),
+    });
+
+    toast.success('Task Completed!');
+  };
+
+  const taskDel = (id?: string) => {
+    setModalOpen(false);
+    setTimeout(async () => {
+      await deleteDoc(doc(db, `rooms/${room?.id}/tasks/${id}`));
+      toast.success('Task Deleted!');
+    }, 300);
+  };
+
   const goToLink = () => {
     toast((t) => (
       <Popup
@@ -55,15 +75,23 @@ const RoomTask: React.FC<RoomTaskProps> = ({
     <div
       className={`w-full relative px-[30px] min-h-[70px] py-4 bg-primary text-secondary rounded transition-all duration-300 group overflow-hidden cursor-default ${padding()}`}
     >
+      <Modal
+        title='Delete Task'
+        description='Are you sure you want to delete this task? This action cannot be undone.'
+        proceed={() => taskDel(task.id)}
+        dismiss={() => setModalOpen(false)}
+        isOpen={modalOpen}
+      />
+
       <SideBtn
-        onClick={() => taskDone(task.id ?? '')}
+        onClick={() => taskDone(task.id)}
         buttonType='check'
         Icon={BsFillCheckCircleFill}
       />
 
       {isAdmin && (
         <SideBtn
-          onClick={() => taskDel(task.id ?? '')}
+          onClick={() => setModalOpen(true)}
           buttonType='close'
           Icon={IoCloseCircle}
           iconStyle='text-xl'
@@ -99,7 +127,7 @@ const RoomTask: React.FC<RoomTaskProps> = ({
               : 'No Due Date'}
           </p>
           <p>
-            Done: {task.completedBy?.length}/{members}
+            Done: {task.completedBy?.length}/{room?.members?.length + 1}
           </p>
         </div>
       </div>
