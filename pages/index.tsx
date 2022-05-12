53cbe294dab3ab4ccd2bb47ff67351205a655acd
Josh Daniel <joshxfi.dev@gmail.com>
2022-05-12 10:32:59 +0800

    feat: update hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,10 @@ import React, { ReactElement } from 'react';
 import { useRouter } from 'next/router';
 import { FaSignInAlt } from 'react-icons/fa';
 
-import avatar from '@/assets/avatar.svg';
+import { Layout } from '@/components';
 import { Button } from '@/components/Button';
 import { landingPage } from '@/utils/constants';
 import { NextPageWithLayout } from 'types/page';
-import { ImageFill, Layout } from '@/components';
 import { useAuth } from '@/contexts/AuthContext';
 
 const Index: NextPageWithLayout = () => {
@@ -15,59 +14,31 @@ const Index: NextPageWithLayout = () => {
 
   return (
     <>
-      <section className='mx-auto flex max-w-screen-xl flex-col items-center lg:mt-20 lg:flex-row lg:items-start lg:justify-between'>
-        <div className='pt-8 text-center lg:flex lg:flex-col lg:items-start lg:justify-between'>
-          <h1 className='text-5xl font-bold md:text-7xl lg:mb-2 xl:text-8xl'>
-            trackAs
-            <span className='gradient-text'>One</span>
-          </h1>
-
-          <div className='flex max-w-screen-xl flex-col items-center lg:items-start lg:pl-2'>
-            <div className='text-center lg:text-left'>
-              <ImageFill
-                contain
-                priority
-                src={avatar}
-                alt='trackAsOne avatar'
-                className='my-8 mx-auto h-[300px] w-[300px] lg:hidden'
-              />
-
-              <h1 className='font-serif text-2xl font-medium xl:w-auto xl:text-3xl'>
-                A free and open-source collaborative task tracker.
-              </h1>
-
-              <p className='text-md my-8 text-lg lg:w-[540px] xl:w-[650px]'>
-                {landingPage.body1}
-              </p>
-            </div>
-
-            <Button
-              onClick={user ? () => push('/home') : signIn}
-              className='welcome-btn'
-              name='get started'
-              Icon={FaSignInAlt}
-            />
-          </div>
+      <section className='mt-20 flex flex-col items-center text-center'>
+        <div className='text-5xl font-semibold md:text-6xl lg:text-7xl'>
+          <h1 className='mb-2 font-serif'>A Collaborative</h1>
+          <h1 className='font-serif'>Task Tracker</h1>
         </div>
 
-        <ImageFill
-          contain
-          priority
-          src={avatar}
-          alt='trackAsOne avatar'
-          className='hidden h-[500px] w-[500px] lg:block'
-        />
+        <p className='my-8 max-w-[700px] md:text-lg'>{landingPage.body1}</p>
+
+        <div className='flex space-x-4'>
+          <Button
+            onClick={user ? () => push('/home') : signIn}
+            className='border-btn'
+            name='Learn more'
+          />
+
+          <Button
+            onClick={user ? () => push('/home') : signIn}
+            className='border-btn'
+            name='Get started'
+            Icon={FaSignInAlt}
+          />
+        </div>
       </section>
 
-      <section className='my-20 border-y-2 border-primary py-5 md:my-40 md:py-12'>
-        <ul className='text-md flex justify-between text-primary text-opacity-90 sm:text-2xl md:text-3xl xl:text-5xl'>
-          {['User-Friendly', 'Collaborative', 'Open-Source'].map((i) => (
-            <li key={i}>
-              <p className='font-serif font-semibold'>{i}</p>
-            </li>
-          ))}
-        </ul>
-      </section>
+      <div className='my-32 h-1 bg-primary' />
 
       <section className='mx-auto max-w-screen-xl space-y-24 pb-40 md:space-y-40'>
         <Info
@@ -96,13 +67,13 @@ interface InfoProps {
 
 const Info = ({ title, body }: InfoProps) => {
   return (
-    <div className='mx-auto rounded border-t-[3px] border-l-[3px] border-r-[6px] border-b-[6px] border-primary bg-white md:w-[700px]'>
-      <div className='h-12 border-b-[3px] border-primary bg-secondary' />
+    <div className='border-effect mx-auto rounded border-primary bg-white md:w-[700px]'>
+      <div className='h-12 border-b-[3px] border-primary bg-lighter' />
       <div className='p-8 text-center md:p-16'>
         <h1 className='mb-4 font-serif text-2xl font-medium md:text-4xl'>
           {title}
         </h1>
-        <p className='text-md mt-2 text-center md:text-lg'>{body}</p>
+        <p className='mt-2 text-center md:text-lg'>{body}</p>
       </div>
     </div>
   );
